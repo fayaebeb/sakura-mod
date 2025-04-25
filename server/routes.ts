5428b464f8d752fbb9ec108cc8bcdb650b3b5b01
fayaebeb <fayaebeb@gmail.com>
2025-04-25 20:24:01 +0900

    skmodnewlf

--- server/routes.ts
+++ server/routes.ts
@@ -169,7 +169,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
           output_type: "chat",
           input_type: "chat",
           tweaks: {
-            "TextInput-ec4Wp": {
+            " TextInput-Ey6nO": {
               input_value: persistentSessionId,
             },
           },
